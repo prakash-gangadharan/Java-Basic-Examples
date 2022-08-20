337fde726cd014acfcec27e0b131ac255a563f16
unknown <cogjava2380@iiht.tech>
2022-08-20 11:15:26 +0530

    removed angular

--- AngularJS-Tutorials/WebRoot/W3SchoolsExamples/1_Intro/scripts/service.js
+++
@@ -1,7 +0,0 @@
-app.service('myService', function(){
-	this.sayHello  = function(){
-		var msg = "hello";
-		console.log(msg);
-		return msg;
-	};
-});
--- AngularJS-Tutorials/WebRoot/javascript_ex/scripts/hello.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("demo").innerHTML = "Fiat";
-document.write("Hello guys , This was written using external script<br>");
-
-document.getElementById("p2").style.color = "red";
