c6a56b46b4d78681ba31a82bed7ddcfb764569ad
prakash-gangadharan <prakash08.g@gmail.com>
2025-07-05 14:57:09 +0530

    moved string based examples

--- Java-All-Examples/src/main/java/com/examples/stringhandling/ChangeCase.java
+++
@@ -1,9 +0,0 @@
-package com.examples.stringhandling;
-
-class ChangeCase {
-	public static void main(String args[]) {
-		String s = "This is a Test String";
-		System.out.println(s.toUpperCase());
-		System.out.println(s.toLowerCase());
-	}
-}
